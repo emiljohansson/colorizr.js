9cf8da8fe9cb96846220fe1f4e0762ad2f5230f3
Emil Johansson <emijohansson@deloitte.com>
2015-07-02 08:06:50 -0600

    add index.html

--- README.md
+++
@@ -1,7 +0,0 @@
-# colorizr.js
-
-Random generator for rgb and hex colors
-
-## Demo
-
-[Check it live!](http://emiljohansson.github.io/colorizr.js)
--- index.js
+++
@@ -1,6 +0,0 @@
-var colorizr = require('./colorizr');
-var rgbValues = colorizr.rgb();
-var hexValue = colorizr.hex();
-console.log(rgbValues);
-console.log(hexValue);
-return hexValue;
